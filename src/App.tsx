9e348910f8217391769bc203a802bacb41874b29
Ben-21 <bs@schaefer-inet.de>
2023-06-22 12:34:55 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import Header from "./components/Header.tsx";
 import CharacterGallery from "./components/CharacterGallery.tsx";
 
 
+
 export default function App() {
+
     return (
+
         <div>
+
             <Header/>
             <CharacterGallery characters={characterList}></CharacterGallery>
         </div>
